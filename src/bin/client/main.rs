64830ae55aff72de701e6cd0da72ca3f0cd49186
federico.viscomi <federico.viscomi@gmail.com>
2022-12-25 07:52:39 +0000

    use select! instead of race just to try it and renamed some functions

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,20 +1,27 @@
-use async_std::io::BufReader;
-use async_std::io::Lines;
-use async_std::io::Stdin;
-use async_std::net::TcpStream;
-use async_std::prelude::*;
-use async_std::task;
-use async_std::io;
-use async_std::net;
-use chat::utils;
-use chat::utils::ChatError;
-use chat::utils::ChatResult;
-use chat::Client;
-use chat::Server;
 use std::ptr::addr_of_mut;
 use std::sync::Arc;
 
-async fn send(mut send: TcpStream) -> ChatResult<()> {
+use async_std::io;
+use async_std::io::BufReader;
+use async_std::io::Lines;
+use async_std::io::Stdin;
+use async_std::net;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
+use async_std::task;
+use futures::{
+    future::FutureExt, // for `.fuse()`
+    pin_mut,
+    select,
+};
+
+use chat::Client;
+use chat::Server;
+use chat::utils;
+use chat::utils::ChatError;
+use chat::utils::ChatResult;
+
+async fn get_command_listener(mut send: TcpStream) -> ChatResult<()> {
     println!("Options \njoin CHAT\npost CHAT MESSAGE\nType CTRL-C to close the connection.");
     let mut options: Lines<BufReader<Stdin>> = BufReader::new(io::stdin()).lines();
     while let Some(option_result) = options.next().await {
@@ -29,7 +36,7 @@ async fn send(mut send: TcpStream) -> ChatResult<()> {
     Ok(())
 }
 
-async fn messages(server: TcpStream) -> ChatResult<()> {
+async fn get_message_receiver(server: TcpStream) -> ChatResult<()> {
     let buf: BufReader<TcpStream> = BufReader::new(server);
     let mut stream = utils::receive(buf);
     while let Some(msg) = stream.next().await {
@@ -84,9 +91,12 @@ fn main() -> ChatResult<()> {
     task::block_on(async {
         let socket: TcpStream = TcpStream::connect(addr).await?;
         socket.set_nodelay(true)?;
-        let send = send(socket.clone());
-        let replies = messages(socket);
-        replies.race(send).await?;
-        Ok(())
+        let command_listener = get_command_listener(socket.clone()).fuse();
+        let message_receiver = get_message_receiver(socket).fuse();
+        pin_mut!(command_listener, message_receiver);
+        select! {
+            result = command_listener => result,
+            result = message_receiver => result
+        }
     })
 }
